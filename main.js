d560e4c57a4a14665251f8f41966eff167518347
2 <1>
2023-09-20 03:44:11 +0200

    adasd

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const container = document.getElementById('container');
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x89CFF0);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+const wireframeMaterial = new THREE.LineBasicMaterial({color: 0x000000});
 const cube = new THREE.LineSegments(new THREE.WireframeGeometry(geometry), wireframeMaterial);
 cube.scale.set(3, 3, 3);
 scene.add(cube);
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, opacity: 0.5, transparent: true });
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00, opacity: 0.5, transparent: true});
 const cube1 = new THREE.Mesh(geometry, material);
 cube1.scale.set(3, 3, 3);
 scene.add(cube1);
@@ -31,12 +31,54 @@ const mouse = new THREE.Vector2();
 const el = document.getElementById("cursor");
 const elWidth = el.offsetWidth;
 const elHeight = el.offsetHeight;
-const { innerWidth: width, innerHeight: height } = window;
-const target = { x: width / 2, y: height / 2 };
-const position = { x: height, y: width };
+const {innerWidth: width, innerHeight: height} = window;
+const target = {x: width / 2, y: height / 2};
+const position = {x: height, y: width};
 const ease = 0.085;
 
+let closable = false;
+
 document.addEventListener('mousemove', onMouseMove);
+
+document.addEventListener('click', () => {
+    const popup = document.getElementById('popup');
+    const close = document.getElementById('close');
+    if (popup.textContent !== ""){
+        close.classList.remove('hidden');
+    } else {
+        close.classList.add('hidden');
+    }
+    if (popup.textContent === 'Projects') {
+        document.getElementById('projects').classList.remove('hidden')
+    } else {
+        document.getElementById('projects').classList.add('hidden')
+    }
+    if (popup.textContent === 'Skills') {
+        document.getElementById('skills').classList.remove('hidden')
+    } else {
+        document.getElementById('skills').classList.add('hidden')
+    }
+    if (popup.textContent === 'Contact') {
+        document.getElementById('contact').classList.remove('hidden')
+    } else {
+        document.getElementById('contact').classList.add('hidden')
+    }
+    if (popup.textContent === 'About Me') {
+        document.getElementById('aboutme').classList.remove('hidden')
+    } else {
+        document.getElementById('aboutme').classList.add('hidden')
+    }
+    if (popup.textContent === 'Certificates') {
+        document.getElementById('certificates').classList.remove('hidden')
+    } else {
+        document.getElementById('certificates').classList.add('hidden')
+    }
+    if (popup.textContent === 'Education / Info') {
+        document.getElementById('education').classList.remove('hidden')
+    } else {
+        document.getElementById('education').classList.add('hidden')
+    }
+});
 
 function onMouseMove(event) {
     target.x = event.clientX;
@@ -57,7 +99,6 @@ function updateCursor() {
 }
 
 
-
 function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
@@ -72,7 +113,7 @@ function animate() {
 
     const popup = document.getElementById('popup');
     const intro = document.getElementById('introduction');
-    if (intersects.length > 0) {
+    if (intersects.length > 0 && !closable) {
         const faceNormal = intersects[0].face.normal;
         if (Math.abs(faceNormal.x) === 1) {
             popup.textContent = faceNormal.x === 1 ? 'Projects' : 'Skills';
@@ -89,6 +130,7 @@ function animate() {
         cursor.style.transform = `scale(${scaleFactor})`;
         cursor.style.backgroundColor = 'red';
     } else {
+        popup.textContent = '';
         popup.classList.add('hidden');
         intro.classList.remove('hidden');
         const cursor = document.getElementById('cursor');
